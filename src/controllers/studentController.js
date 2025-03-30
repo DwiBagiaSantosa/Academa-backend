29c7c41b3aa9c23d2cc31ac6fb3348dd987a877a
Dwi Bagia Santosa <dwibagiasentosa@gmail.com>
2025-03-30 20:12:05 +0700

    create: update students api

--- src/controllers/studentController.js
+++ src/controllers/studentController.js
@@ -7,11 +7,18 @@ export const getStudents = async (req, res) => {
         const students = await userModel.find({
             role: "student",
             manager: req.user._id
+        }).select('name courses photo')
+
+        const response = students.map((item) => {
+            return {
+                ...item.toObject(),
+                photo_url: process.env.APP_URL + '/uploads/students/' + item.photo
+            }
         })
 
         return res.json({
             message: "Get students success",
-            data: students
+            data: response
         })
     } catch (error) {
         console.log("🚀 ~ getStudents ~ error:", error)
@@ -54,6 +61,54 @@ export const createStudent = async (req, res) => {
 
         return res.json({
             message: "Create student success",
+        })
+    } catch (error) {
+        console.log("🚀 ~ createStudent ~ error:", error)
+        return res.status(500).json({ error: "Internal Server Error" })
+    }
+}
+
+export const updateStudent = async (req, res) => {
+    try {
+        const { id } = req.params
+        const body = req.body
+
+        const parse = mutateStudentSchema.partial({
+            password: true
+        }).safeParse(body)
+
+        if (!parse.success) {
+            const errorMessage = parse.error.issues.map((err) => err.message)
+
+            if (req?.file.path && fs.existsSync(req?.file?.path)) {
+                fs.unlinkSync(req?.file?.path)
+            }
+
+            return res.status(500).json({
+                message: "Error Validation",
+                data: null,
+                error: errorMessage
+            })
+        }
+
+        const student = await userModel.findById(id)
+
+        const hashPassword = parse.data?.password ? bcrypt.hashSync(parse.data.password, 12) : student.password
+
+        const updatedStudent = await userModel.findByIdAndUpdate(id, {
+            name: parse.data.name,
+            email: parse.data.email,
+            password: hashPassword,
+            photo: req.file ? req.file?.filename : student.photo,
+        }, { new: true })
+
+        
+
+        // await student.save()
+
+        return res.json({
+            message: "Update student success",
+            data: updatedStudent
         })
     } catch (error) {
         console.log("🚀 ~ createStudent ~ error:", error)
--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createStudent, getStudents } from "../controllers/studentController.js";
+import { createStudent, getStudents, updateStudent } from "../controllers/studentController.js";
 import { verifyToken } from "../middlewares/verifyToken.js";
 import multer from "multer";
 import { fileFilter, fileStorage } from "../utils/multer.js";
@@ -14,5 +14,7 @@ const upload = multer({
 studentRoutes.get('/students', verifyToken, getStudents)
 
 studentRoutes.post('/students', verifyToken, upload.single('avatar'), createStudent)
+
+studentRoutes.put('/students/:id', verifyToken, upload.single('avatar'), updateStudent)
 
 export default studentRoutes
